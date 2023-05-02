7c901f568495960b29cf47bf40dfb210f4d99f29
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-05-02 11:48:58 +0200

    router marche

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ $router = new AltoRouter();
 
 $router->setBasePath('/super-week');
 
-$router->map('GET', '/', function (){
-    require_once 'home.html';
+$router->map('GET', '/', function()  {
+    require_once 'home.php';
 }, 'home' );
 
+
+$match = $router->match();
+
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
 ?>
