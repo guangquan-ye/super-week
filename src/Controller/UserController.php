e51c6c0c4a0e176cc9f8727cff1ffc74ee572496
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-05-03 13:33:33 +0200

    add register.php router index

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,10 +13,26 @@ Class UserController{
         $this->user = new UserModel();
     }
 
+    public function createUsers($email, $password, $firstname, $lastname, $passwordConf)
+    {
+
+        if($password == $passwordConf){
+            
+            if($this->user->verifyExist($email)){
+
+            }else{
+                $this->user->insert($email, $password, $firstname, $lastname);
+            }
+        }
+    }
     public function findAll()
     {
+        
         return $this->user->selectAll();
     }
-}
 
-?>
+    public function regFormDisplay(){
+
+        require_once "./src/View/register.php";
+    }
+}
