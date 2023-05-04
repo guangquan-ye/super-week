9bb63df62297f9a3131a5de8f85f0326f00e483c
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-05-04 10:03:56 +0200

    add feature addbook

---
+++ src/View/addbook.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <input type="text" name="addBookTitle" placeholder="Title">
+    <input type="text" name="addBookContent" placeholder="Content">
+    <button type="submit" name="addBookBtn">Submit</button>
+</form>
